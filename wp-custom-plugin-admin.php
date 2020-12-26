e906fb1a09c67500a77f9b0a50dc8a08f4c23d70
rmanescau <rmanescau@etiskapp.com>
2020-12-26 13:29:12 +0100

    add description

---
+++ wp-custom-plugin-admin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Custom Plugin Admin
+Plugin URI: https://github.com/goomies/wp-custom-plugin-admin
+Minimalist Wordpress plugin for a personalized administration interface.
+Author: Rémy Manescau @goomies (GitHub - GitLab)
+Author URI: https://www.remymanescau.com/
+Version: 1.0
+*/
